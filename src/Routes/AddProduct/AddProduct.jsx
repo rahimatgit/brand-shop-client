ce6b7a2eaff340801fcecdfc7b51bd2d8a097ea6
rahimatgit <rahimmonjur9@gmail.com>
2023-10-20 15:44:39 -0700

    add product section created

--- src/Routes/AddProduct/AddProduct.jsx
+++ src/Routes/AddProduct/AddProduct.jsx
@@ -1,9 +1,103 @@
-
+import {
+    Card,
+    Input,
+    Checkbox,
+    Button,
+    Typography,
+} from "@material-tailwind/react";
 
 const AddProduct = () => {
     return (
-        <div>
-            <h1>add your product.</h1>
+        <div className="w-full md:w-[40%] mt-12 mx-auto rounded-2xl flex justify-center bg-green-100 px-6 py-12">
+            <Card color="transparent" shadow={false}>
+                <Typography variant="h4" color="blue-gray">
+                    Add Your Product
+                </Typography>
+                
+                <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+                    <div className="mb-1 flex flex-col gap-6">
+                        <Typography variant="h6" color="blue-gray" className="-mb-3">
+                            Brand Name
+                        </Typography>
+                        <Input
+                            name="brand-name"
+                            size="lg"
+                            placeholder="brand name"
+                            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+                            labelProps={{
+                                className: "before:content-none after:content-none",
+                            }}
+                        />
+                        <Typography variant="h6" color="blue-gray" className="-mb-3">
+                            Product Name
+                        </Typography>
+                        <Input
+                            name="product-name"
+                            size="lg"
+                            placeholder="product name"
+                            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+                            labelProps={{
+                                className: "before:content-none after:content-none",
+                            }}
+                        />
+                        <Typography variant="h6" color="blue-gray" className="-mb-3">
+                            Type
+                        </Typography>
+                        <Input
+                            name="type"
+                            size="lg"
+                            placeholder="type"
+                            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+                            labelProps={{
+                                className: "before:content-none after:content-none",
+                            }}
+                        />
+                        <Typography variant="h6" color="blue-gray" className="-mb-3">
+                            Image
+                        </Typography>
+                        <Input
+                            name="image"
+                            size="lg"
+                            placeholder="image"
+                            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+                            labelProps={{
+                                className: "before:content-none after:content-none",
+                            }}
+                        />
+                        <Typography variant="h6" color="blue-gray" className="-mb-3">
+                            Price
+                        </Typography>
+                        <Input
+                            name="price"
+                            size="lg"
+                            placeholder="price"
+                            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+                            labelProps={{
+                                className: "before:content-none after:content-none",
+                            }}
+                        />
+                        <Typography variant="h6" color="blue-gray" className="-mb-3">
+                            Short Description
+                        </Typography>
+                        <Input
+                            name="description"
+                            size="lg"
+                            placeholder="short description"
+                            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+                            labelProps={{
+                                className: "before:content-none after:content-none",
+                            }}
+                        />
+                        
+                        
+                    </div>
+                   
+                    <Button className="mt-6" fullWidth>
+                    <input type="submit" value="Add Product" />
+                    </Button>
+
+                </form>
+            </Card>
         </div>
     );
 };
